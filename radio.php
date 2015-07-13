2aabdc7bd39d7684b097efcb06c6322efa079dce
Swas Kunakorn <zeemyself@gmail.com>
2015-07-13 14:06:57 +0700

    Fix radio

--- radio.php
+++ radio.php
@@ -1,7 +1,8 @@
 <!DOCTYPE HTML>
+<html>
 <head>
   My Radio
 </head>
-<html>
+
 <iframe src="http://www.bakaradio.net/musicbox/bakaworld/radio.php?anime=4" width="960" height="200" frameborder="0" scrolling="no"></iframe>
 </html>
